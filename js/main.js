941f97bfeb560c80f9f168d3b82536fa0b917cd0
Saskia Peach <saskiapeach@gmail.com>
2022-09-15 16:38:45 +0100

    implement object collision and game over

--- js/main.js
+++ js/main.js
@@ -1,11 +1,45 @@
 class Game {
     constructor(){
         this.player = null; //will store an instance of the class Player
+        this.obstacles = []; //will store instance of the class obstacle
     }
+
     start(){
         this.player = new Player();
         this.attachEventListeners();
+
+         //create new obstacle
+         setInterval(() => {
+           
+            const newObstacle = new Obstacle();
+            this.obstacles.push(newObstacle);
+        }, 3000);
+        
+
+   //move obstacles
+   setInterval(() => {
+    this.obstacles.forEach( (obstacleInstance) => {
+
+        //move
+        obstacleInstance.moveDown();
+
+        //detect collision
+        if (
+            this.player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            this.player.positionX + this.player.width > obstacleInstance.positionX &&
+            this.player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            this.player.height + this.player.positionY > obstacleInstance.positionY
+        ) {
+            location.href = "gameover.html"
+        }
+
+    });
+}, 60);
+
+       
     }
+
+
     attachEventListeners(){
         document.addEventListener("keydown", (event) => {
             if(event.key === "ArrowLeft"){
@@ -43,14 +77,55 @@ class Player {
         boardElm.appendChild(this.domElement)
     }
     moveLeft(){
-        this.positionX--;
+        this.positionX-=2;
         this.domElement.style.left = this.positionX + "vw";
+
     }
     moveRight(){
-        this.positionX++;
+        this.positionX+=2;
         this.domElement.style.left = this.positionX + "vw";
     }
 }
+
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 90;
+        this.width = 10;
+        this.height = 10;
+        this.domElement = null;
+        this.createDomElement()
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement('div');
+
+        // set id and css
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+
+        // append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
+    }
+
+
+    moveDown(){
+        this.positionY--;
+        this.domElement.style.bottom = this.positionY + "vh";
+
+    }
+    
+}
+
+
+
+
+
+
 
 
 
